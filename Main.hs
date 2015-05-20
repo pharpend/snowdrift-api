85eb1575f1bfc41e60939c755d7363f627ac867d
Peter Harpending <peter@harpending.org>
2015-05-19 22:52:40 -0600

    Add a main.hs with main = return ()

---
+++ Main.hs
@@ -0,0 +1,33 @@
+-- snowdrift-api - Server for Snowdrift API.
+-- Copyright (C) 2015 Snowdrift.coop
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-- | 
+-- Module      : Main
+-- Description : Runs the server software
+-- Copyright   : Copyright (C) 2015 Snowdrift.coop
+-- License     : AGPL-3
+-- Maintainer  : dev@lists.snowdrift.coop
+-- Stability   : experimental
+-- Portability : POSIX
+-- 
+-- Here is a longer description of this module, containing some
+-- commentary with @some markup@.
+
+module Main where
+
+-- |Main is null for now
+main :: IO ()
+main = return ()
