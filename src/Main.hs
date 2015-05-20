c9c56674a83e65b6fe4db68bde2b4bbf3b02a29e
Peter Harpending <peter@harpending.org>
2015-05-20 16:14:18 -0600

    Server now sends back body of request.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- snowdrift-api - Server for Snowdrift API.
 -- Copyright (C) 2015 Snowdrift.coop
 --
@@ -23,12 +27,79 @@
 -- Maintainer  : peter@harpending.org
 -- Stability   : experimental
 -- Portability : POSIX
--- 
--- Here is a longer description of this module, containing some
--- commentary with @some markup@.
 
 module Main where
 
+import Control.Monad.Trans
+import Control.Monad.Trans.Resource
+import Data.Conduit
+import qualified Data.Conduit.Combinators as CC
+import Data.List
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.IO as TIO
+import Data.Version (showVersion)
+import Network.HTTP.Types.Status
+import Network.Wai
+import Network.Wai.Conduit
+import Network.Wai.Handler.Warp
+import Paths_snowdrift_api
+import Safe
+import System.Pager
+import System.Posix.ByteString hiding (version)
+
+#if !(MIN_VERSION_base(4,8,0))
+import Data.Monoid
+#endif
+
 -- |Main is null for now
 main :: IO ()
-main = return ()
+main =
+  do cmdargs <- fmap (fmap TE.decodeUtf8) getArgs
+     if |  or [elem "-h" cmdargs,elem "--help" cmdargs] -> printOrPage helpPage
+        |  elem "--version" cmdargs ->
+          putStrLn (showVersion version)
+        |  elem "--license" cmdargs ->
+          getDataFileName "LICENSE" >>=
+          TIO.readFile >>=
+          printOrPage
+        |  otherwise ->
+          runArgs (Args (case findIndex (\x ->
+                                           or [(x == "--port"),(x == "-p")])
+                                        cmdargs of
+                           Nothing -> 8778
+                           Just x ->
+                             read (T.unpack (at cmdargs (x + 1)))))
+
+-- |The help page
+helpPage :: Text
+helpPage =
+  T.unlines [(mappend "snowdrift-api v." (T.pack (showVersion version)))
+            ,"Written by Peter Harpending."
+            ,"Copyright (c) 2015 Snowdrift.coop. See `snowdrift-api --license` for the full license."
+            ,mempty
+            ,"OPTIONS"
+            ,T.stripEnd
+               (T.unlines (fmap (mappend (T.replicate 4 " "))
+                                ["-h,--help                         Show this page"
+                                ,"--license                         Print out the license (AGPLv3+)."
+                                ,"--version                         Print out the version."
+                                ,"-p,--port PORT                    Port on which to run the server (default: 8778)"]))]
+
+-- |After taking care of the trivial stuff, the arguments are marshaled
+-- into this type.
+data Args =
+  Args {port :: Int}
+
+-- |Take an 'Args' and run with it
+runArgs :: Args -> IO ()
+runArgs (Args port_) = run port_ app
+
+-- |At the moment, this just sends back the request body
+app :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived 
+app request responseHandler =
+  runResourceT
+    (do requestBody_ <- connect (sourceRequestBody request) CC.sinkLazy
+        let response_ = responseLBS status200 mempty requestBody_
+        lift (responseHandler response_))
